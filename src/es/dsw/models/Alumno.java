a322d1afca08e62e0b8ef8d1424c68697edfa246
‘alumno-ets’ <alumno-ets@alumno.ieselrincon.es>
2024-06-11 19:59:59 +0100

    Modificación 2

---
+++ src/es/dsw/models/Alumno.java
@@ -0,0 +1,10 @@
+package es.dsw.models;
+
+public class Alumno extends Profesor {
+
+	public Alumno(String nombre, String appelido1, String apellido2, String nif) {
+		super(nombre, appelido1, apellido2, nif);
+		// TODO Auto-generated constructor stub
+	}
+
+}
